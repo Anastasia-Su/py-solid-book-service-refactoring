57755f56744df5ac428ff5853d7a78cfee14e182
Anastasia <anastasiiya.p@gmail.com>
2024-04-01 19:08:06 +0300

    Fix flake8

--- app/serializer.py
+++ app/serializer.py
@@ -1,5 +1,5 @@
 import json
-import xml.etree.ElementTree as ET
+import xml.etree.ElementTree as ElementTree
 from book import Book
 
 
@@ -10,9 +10,9 @@ class Serializer:
 
     @staticmethod
     def xml(book: Book) -> str:
-        root = ET.Element("book")
-        title = ET.SubElement(root, "title")
+        root = ElementTree.Element("book")
+        title = ElementTree.SubElement(root, "title")
         title.text = book.title
-        content = ET.SubElement(root, "content")
+        content = ElementTree.SubElement(root, "content")
         content.text = book.content
-        return ET.tostring(root, encoding="unicode")
+        return ElementTree.tostring(root, encoding="unicode")
