45bbe0c118c1e15d5fa80a2c5baf255e634d2379
Anastasia <anastasiiya.p@gmail.com>
2024-04-01 21:41:08 +0300

    Fix flake8

--- app/print.py
+++ app/print.py
@@ -4,8 +4,13 @@ from app.book import Book
 class Print:
     @staticmethod
     def console(book: Book) -> None:
-        print(f"Printing the book: {book.title}...\n{book.content}")
+        print(
+            f"Printing the book: {book.title}...\n{book.content}"
+        )
 
     @staticmethod
     def reverse(book: Book) -> None:
-        print(f"Printing the book in reverse: {book.title}...\n{book.content[::-1]}")
+        print(
+            f"Printing the book in reverse: {book.title}...\n"
+            f"{book.content[::-1]}"
+        )
